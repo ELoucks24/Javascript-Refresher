a4eb559cae99fdeb3ed88e26a1928259c1729a54
Eloucks24 <eloucks24@penguinhall.org>
2022-10-24 11:10:17 -0400

    day 1 progress

---
+++ refresher.js
@@ -0,0 +1,14 @@
+// console.log('hello world!');
+let message = 'How are you today?';
+// console.log(message);
+let greeting = "Hello";
+let name = "Bob";
+let text3 = greeting + " " + name;
+// console.log(text3);
+let age = 13
+// console.log('you are ' + age);
+let futureAge = age + 10
+// console.log('In ten years you will be ' + futureAge);
+// // part 2
+if name = 'Bob'; 
+    console.log('i knew it was you, bob!');
