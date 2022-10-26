1b7b1187489a4cafef6572cdca14d010bde70dad
Eloucks24 <eloucks24@penguinhall.org>
2022-10-26 09:45:42 -0400

    work from today

--- refresher.js
+++ refresher.js
@@ -1,14 +1,54 @@
-// console.log('hello world!');
-let message = 'How are you today?';
-// console.log(message);
-let greeting = "Hello";
-let name = "Bob";
-let text3 = greeting + " " + name;
-// console.log(text3);
-let age = 13
-// console.log('you are ' + age);
-let futureAge = age + 10
-// console.log('In ten years you will be ' + futureAge);
-// // part 2
-if name = 'Bob'; 
-    console.log('i knew it was you, bob!');
+// // console.log('hello world!');
+// let message = 'How are you today?';
+// // console.log(message);
+// let greeting = 'Hello';
+// let name = 'Sue';
+// let text3 = greeting + " " + name;
+// // console.log(text3);
+// let age = 13
+// // console.log('you are ' + age);
+// let futureAge = age + 10
+// // console.log('In ten years you will be ' + futureAge);
+// // // part 2
+// // if (name = 'Bob') {
+// //     console.log('i knew it was you Bob!')
+// //   }
+
+// let iAMWearingABelt = true
+// let iAmWearingGlasses = false
+// if (iAMWearingABelt == true && iAmWearingGlasses == false) {
+//     console.log('Good morning');
+//   } else if (iAMWearingABelt == true && iAmWearingGlasses == true) {
+//     console.log('Good day');
+//   } else {
+//     console.log('Good evening');
+//   }
+//     tellMyAge(14)
+
+//   function tellMyAge (hippo){
+//     console.log ('you are ' + hippo + ' years old');
+// }
+
+
+// function makePizza (type, size){
+//     console.log ('One ' + size + ' ' + type + ' pizza coming up!')
+// }
+
+// makePizza ('pepperoni', 'small')
+
+function pricePizza(size) {
+    if (size == 'large') {
+        return 17
+    } else {
+        return 13
+    }
+}
+
+let x = pricePizza('small')
+console.log (x)
+
+
+
+
+
+
