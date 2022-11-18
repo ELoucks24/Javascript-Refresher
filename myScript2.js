7f6b15f255aff8f0362259bd8ad4f0ebfda3b824
Eloucks24 <eloucks24@penguinhall.org>
2022-11-18 08:05:20 -0500

    homework

---
+++ myScript2.js
@@ -0,0 +1,15 @@
+function submitInput(){
+
+    let name = document.getElementById('name').value
+    let animal = document.getElementById('animal').value
+
+console.log('hello ' + name + '.Dont you wish you were a ' + animal + '?')
+}
+function changeColor(){
+
+    let name = document.getElementById('name').style.backgroungColor = "red"
+   
+
+
+}
+console.log('hello ' + name + '.Dont you wish you were a ' + animal + '?')
